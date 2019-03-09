0c9fe1b6ed60068e35301ef026c936930395b1c9
fenglimin <rowstatus@163.com>
2019-03-09 22:07:32 +0800

    Update dropdown-button-menu-button.component.ts

--- Csh.ImageSuite.WebClient/src/app/components/common/dropdown-button-menu-button/dropdown-button-menu-button.component.ts
+++ Csh.ImageSuite.WebClient/src/app/components/common/dropdown-button-menu-button/dropdown-button-menu-button.component.ts
@@ -11,6 +11,9 @@ import { ConfigurationService } from '../../../services/configuration.service';
 export class DropdownButtonMenuButtonComponent implements OnInit {
   buttonStyleToken: ButtonStyleToken;
   baseUrl: string;
+  isCheckStyle: boolean;
+  isChecked: boolean;
+  defaultStyle: string;
 
   @Output() selected = new EventEmitter<SelectedButtonData>();
 
@@ -24,10 +27,19 @@ export class DropdownButtonMenuButtonComponent implements OnInit {
     }
 
   ngOnInit() {
+    const op = this.buttonData.operationData.type;
+    this.isCheckStyle = (op == OperationEnum.ToggleKeyImage || op == OperationEnum.ShowAnnotation || op == OperationEnum.ShowGraphicOverlay ||
+      op == OperationEnum.ShowOverlay || op == OperationEnum.ShowRuler);
+    this.isChecked = (op == OperationEnum.ShowAnnotation || op == OperationEnum.ShowOverlay || op == OperationEnum.ShowRuler);;
+    this.defaultStyle = this.getDefaultStyle();
+  }
+
+  getDefaultStyle():string {
+    return this.isChecked ? this.buttonStyleToken.down : this.buttonStyleToken.normal;
   }
 
   getDefaultBackgroundUrl(): string {
-      return this.createBackgroundUrl(this.buttonStyleToken.normal);
+    return this.createBackgroundUrl(this.defaultStyle);
   }
 
   getDefaultMarginTop() {
@@ -41,24 +53,33 @@ export class DropdownButtonMenuButtonComponent implements OnInit {
   }
 
   onMouseOver(event) {
-      event.target.style.backgroundImage = this.createBackgroundUrl(this.buttonStyleToken.over);
+    this.setBackgroundImage(event, this.buttonStyleToken.over);
   }
 
   onMouseOut(event) {
-      event.target.style.backgroundImage = this.createBackgroundUrl(this.buttonStyleToken.normal);
+    this.setBackgroundImage(event, this.defaultStyle);
   }
 
   onMouseDown(event) {
-      event.target.style.backgroundImage = this.createBackgroundUrl(this.buttonStyleToken.down);
+    this.setBackgroundImage(event, this.buttonStyleToken.down);
   }
 
   onMouseUp(event) {
-    event.target.style.backgroundImage = this.createBackgroundUrl(this.buttonStyleToken.normal);
+    this.setBackgroundImage(event, this.defaultStyle);
+  }
+
+  setBackgroundImage(event, style) {
+    event.target.style.backgroundImage = this.createBackgroundUrl(style);
   }
 
   onClick() {
       this.selected.emit(this.buttonData);
     this.viewContext.onOperation(this.buttonData.operationData);
-  }
 
+    if (this.isCheckStyle) {
+      this.isChecked = !this.isChecked;
+      this.setBackgroundImage(event, this.isChecked ? this.buttonStyleToken.down : this.buttonStyleToken.normal);
+      this.defaultStyle = this.getDefaultStyle();
+    }
+  }
 }
