12c90be888da82ed38bd10cc42d93510c20ecffe
fenglimin <rowstatus@163.com>
2019-05-19 14:35:10 +0800

    Support polygon

--- Csh.ImageSuite.WebClient/src/lib/jCanvaScript.js
+++ Csh.ImageSuite.WebClient/src/lib/jCanvaScript.js
@@ -1639,6 +1639,14 @@
     proto.shape.prototype = new proto.object;
 
     proto.lines = function () {
+        this.draw = function (ctx) {
+            if (this._x0 === undefined) return;
+            ctx.moveTo(this._x0, this._y0);
+            for (var j = 1; j < this.shapesCount; j++) {
+                ctx.lineTo(this['_x' + j], this['_y' + j]);
+            }
+            ctx.lineTo(this._x0, this._y0);
+        }
         this.getCenter = function (type) {
             var point = {
                 x: this._x0,
